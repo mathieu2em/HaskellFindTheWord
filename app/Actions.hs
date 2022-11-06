acb89799f799de2e8d52e97bb9fdd459a28748a6
mathieu perron <math.per@protonmail.com>
2022-11-06 14:43:32 -0500

    find the word main MVP working

--- app/Actions.hs
+++ app/Actions.hs
@@ -1,17 +1,18 @@
 module Actions where
+
 -- Additional IO Actions functions are gonna be here.
 -- verify if the letter is part of the word
 askDifficultyLevel :: IO String
 askDifficultyLevel = askDifficultyLevel' False
     
-
+-- ask the level of difficulty to the user
 askDifficultyLevel' :: Bool -> IO String
 askDifficultyLevel' b = do
         
         let errorMessage = "Invalid Option. valid options are 1, 2 or 3.\n"
         let message = "Choose the difficulty level : \n write 1 for easy, 2 for medium, 3 for hard."
         
-        if b == True then do
+        if b then do
             putStrLn (errorMessage ++ message)
         else do
             putStrLn message
@@ -25,3 +26,40 @@ askDifficultyLevel' b = do
                 askDifficultyLevel' True
         else do
             askDifficultyLevel' True
+
+guessLetter :: IO Char
+guessLetter = do 
+    putStrLn "Enter a letter to guess the word"
+    head <$> getLine
+    -- TODO validate length and ask again as long as needed
+
+findTheWordGame :: String -> IO ()
+findTheWordGame wordToFind = findTheWordGame' (map (const '-') wordToFind) wordToFind []
+
+findTheWordGame' :: String -> String -> [Char] -> IO () 
+findTheWordGame' foundLetters completeWord alreadyUsedLetters = do
+    putStrLn foundLetters
+    putStrLn ("already used letters : " ++ alreadyUsedLetters)
+    -- validation checkers (length etc.)
+    -- TODO
+    -- check if letter is in the word
+    letter <- guessLetter
+    -- setup the function to create the partial word from the context and guessed letter
+    let createPartialWordWithContext = createPartialWord completeWord foundLetters letter
+    if letter `elem` completeWord then do
+        putStrLn ("The letter \"" ++ [letter] ++"\" is in the word!")
+        -- reveal the letters by modifying the foundLetters word
+        let newFoundLetters = zipWith createPartialWordWithContext [1..] completeWord
+        findTheWordGame' newFoundLetters completeWord (letter:alreadyUsedLetters)
+    else 
+        findTheWordGame' foundLetters completeWord (letter:alreadyUsedLetters)
+
+createPartialWord :: String -> String -> Char -> Int -> Char -> Char
+createPartialWord completeWord oldPartialWord guessedLetter index letter =
+    if getCharFromString index completeWord == guessedLetter 
+        then letter 
+    else getCharFromString index oldPartialWord
+
+-- Allow us to get the char at index n of a string
+getCharFromString :: Int -> String -> Char
+getCharFromString n word = last (take n word)
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Actions (askDifficultyLevel)
+import Actions (askDifficultyLevel, findTheWordGame)
 
 main :: IO ()
 main = do
@@ -12,14 +12,17 @@ main = do
     wordToFind <- getLine
     putStrLn ("you wrote :" ++ wordToFind)
 
+    findTheWordGame wordToFind
+
     -- now ask the user to write a single letter
     putStrLn "Write a letter and the program will let you know if its part of the word"
     letter <- getLine
+    -- show the letter to the user
     putStrLn ("you chose the char " ++ letter )
     if letterIsInWord (head letter) wordToFind
         then putStrLn " the letter was part of the word" 
     else putStrLn " the letter isnt part of the word"
-    -- show the letter to the user
+    
         
 
 letterIsInWord :: Char -> String -> Bool
